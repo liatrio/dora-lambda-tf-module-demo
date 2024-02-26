9f4e3afc725da03c594375015048af8618bafc53
Casey Wilson <casey.wilson@liatrio.com>
2024-02-26 14:47:19 -0500

    feat: add timestamp api endpoint

---
+++ src/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.21.6
+
+require github.com/aws/aws-lambda-go v1.46.0
--- src/lambda_function.py
+++
@@ -1,7 +0,0 @@
-import boto3
-def lambda_handler(event, context):
-    result = "Hello World"
-    return {
-        'statusCode' : 200,
-        'body': result
-    }
